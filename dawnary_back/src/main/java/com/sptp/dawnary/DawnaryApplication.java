1efac13ebfa46f65e6a41f30fbdfae977faf8b25
prao <101629085+dev-prao@users.noreply.github.com>
2024-06-15 19:27:31 +0900

    Init: "초기 프로젝트 구조 추가"
    의존성 및 초기 프로젝트 구조 추가

---
+++ dawnary_back/src/main/java/com/sptp/dawnary/DawnaryApplication.java
@@ -0,0 +1,13 @@
+package com.sptp.dawnary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawnaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawnaryApplication.class, args);
+	}
+
+}
