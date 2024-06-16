83a52f24f4440f77a374d82b0bb3e05c99cc04c0
prao <pch5521@gmail.com>
2024-06-16 22:40:37 +0900

    Feat: "Member 수정 메서드 추가"

--- dawnary_back/src/main/java/com/sptp/dawnary/member/domain/Member.java
+++ dawnary_back/src/main/java/com/sptp/dawnary/member/domain/Member.java
@@ -4,10 +4,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
 
 	@Id
@@ -15,11 +21,27 @@ public class Member {
 	@Column(name = "member_id")
 	private Long id;
 
+	@Column(name = "email", length = 50, updatable = false, unique = true)
 	private String email;
 
+	@Column(name = "password", nullable = false)
 	private String password;
 
+	@Column(name = "name", nullable = false)
 	private String name;
 
+	@Column(name = "image_path", nullable = false)
 	private String imagePath;
+
+	public void updatePassword(String password) {
+		this.password = password;
+	}
+
+	public void updateName(String name) {
+		this.name = name;
+	}
+
+	public void updateImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
 }
