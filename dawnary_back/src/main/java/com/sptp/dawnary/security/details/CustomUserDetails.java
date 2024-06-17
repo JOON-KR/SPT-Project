876b80e60b1971752067a8e1fece27add2711643
prao <pch5521@gmail.com>
2024-06-17 21:24:46 +0900

    Feat: "CustomUserDetails 추가"

---
+++ dawnary_back/src/main/java/com/sptp/dawnary/security/details/CustomUserDetails.java
@@ -0,0 +1,61 @@
+package com.sptp.dawnary.security.details;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.sptp.dawnary.member.dto.CustomUserInfoDto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+	private final CustomUserInfoDto member;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<String> roles = new ArrayList<>();
+		roles.add("ROLE_" + member.getRole().toString());
+
+		return roles.stream()
+			.map(SimpleGrantedAuthority::new)
+			.toList();
+	}
+
+	@Override
+	public String getPassword() {
+		return member.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return member.getMemberId().toString();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
