48f2e85a432f5d0bad3db066d764007d2ca1b264
prao <pch5521@gmail.com>
2024-06-18 00:34:21 +0900

    Feat: "회원가입 기능 추가"

--- dawnary_back/src/main/java/com/sptp/dawnary/member/service/MemberService.java
+++ dawnary_back/src/main/java/com/sptp/dawnary/member/service/MemberService.java
@@ -1,5 +1,7 @@
 package com.sptp.dawnary.member.service;
 
+import java.util.Optional;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,6 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sptp.dawnary.exception.ValidateMemberException;
 import com.sptp.dawnary.member.domain.Member;
 import com.sptp.dawnary.member.dto.CustomUserInfoDto;
 import com.sptp.dawnary.member.dto.LoginRequestDto;
@@ -29,18 +32,31 @@ public class MemberService {
 	public String login(LoginRequestDto dto) {
 		String email = dto.getEmail();
 		String password = dto.getPassword();
-		Member member = memberRepository.findMemberByEmail(email);
-		if (member == null) {
+		Optional<Member> member = memberRepository.findMemberByEmail(email);
+		if (member.isEmpty()) {
 			throw new UsernameNotFoundException("이메일이 존재하지 않습니다.");
 		}
 
 		//암호화된 password를 디코딩한 값과 입력한 패스워드 값이 다르면 null 반환
-		if (!encoder.matches(password, member.getPassword())) {
+		if (!encoder.matches(password, member.get().getPassword())) {
 			throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
 		}
 
 		CustomUserInfoDto info = modelMapper.map(member, CustomUserInfoDto.class);
 		return jwtUtil.createAccessToken(info);
+	}
+
+	@Transactional
+	public Long signup(Member member) {
+		Optional<Member> validMember = memberRepository.findMemberByEmail(member.getEmail());
+
+		if (validMember.isPresent()) {
+			throw new ValidateMemberException("this member email is already exist. " + member.getEmail());
+		}
+		//비밀번호 해시 처리
+		member.updatePassword(encoder.encode(member.getPassword()));
+		memberRepository.create(member);
+		return member.getId();
 	}
 }
 
