e8d7f821ee9e888816f4aaf03138833f3f9e5e33
prao <pch5521@gmail.com>
2024-06-17 21:24:22 +0900

    Feat: "SecurityConfig 추가"

---
+++ dawnary_back/src/main/java/com/sptp/dawnary/security/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.sptp.dawnary.security.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@AllArgsConstructor
+public class SecurityConfig {
+
+
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+}
