1e55d2393d7b2db4689bf90862cc30081ef48203
prao <pch5521@gmail.com>
2024-06-18 00:33:13 +0900

    Feat: "회원가입 기능 추가"

--- dawnary_back/src/main/java/com/sptp/dawnary/member/controller/MemberController.java
+++ dawnary_back/src/main/java/com/sptp/dawnary/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.sptp.dawnary.member.controller;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sptp.dawnary.member.domain.Member;
 import com.sptp.dawnary.member.dto.LoginRequestDto;
+import com.sptp.dawnary.member.dto.MemberRequestDto;
 import com.sptp.dawnary.member.service.MemberService;
 
 import jakarta.validation.Valid;
@@ -19,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 public class MemberController {
 
 	private final MemberService memberService;
+	private final ModelMapper modelMapper;
 
 	@PostMapping("login")
 	public ResponseEntity<String> getMemberProfile(
@@ -26,5 +30,12 @@ public class MemberController {
 	) {
 		String token = memberService.login(request);
 		return ResponseEntity.status(HttpStatus.OK).body(token);
+	}
+
+	@PostMapping("signup")
+	public ResponseEntity<Long> signup(@Valid @RequestBody MemberRequestDto member) {
+		Member entity = modelMapper.map(member, Member.class);
+		Long id = memberService.signup(entity);
+		return ResponseEntity.status(HttpStatus.OK).body(id);
 	}
 }
