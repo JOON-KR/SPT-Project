7916200e7114d35b249cf0ca971d48cbad17ff2c
prao <pch5521@gmail.com>
2024-06-18 00:33:39 +0900

    Refactor: "findMemberByEmail 로직 변경"

--- dawnary_back/src/main/java/com/sptp/dawnary/member/repository/MemberRepository.java
+++ dawnary_back/src/main/java/com/sptp/dawnary/member/repository/MemberRepository.java
@@ -1,12 +1,15 @@
 package com.sptp.dawnary.member.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
 import com.sptp.dawnary.member.domain.Member;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
 
 @Repository
@@ -15,7 +18,7 @@ public class MemberRepository {
 
 	private final EntityManager em;
 
-	public void save(Member member) {
+	public void create(Member member) {
 		em.persist(member);
 	}
 
@@ -23,8 +26,16 @@ public class MemberRepository {
 		return em.find(Member.class, id);
 	}
 
-	public Member findMemberByEmail(String email) {
-		return em.find(Member.class, email);
+	public Optional<Member> findMemberByEmail(String email) {
+		TypedQuery<Member> typedQuery = em.createQuery(
+			"select m from Member m where m.email = :email", Member.class);
+		typedQuery.setParameter("email", email);
+		try {
+			Member member = typedQuery.getSingleResult();
+			return Optional.ofNullable(member);
+		} catch (NoResultException e) {
+			return Optional.empty();
+		}
 	}
 
 	public List<Member> findAll() {
