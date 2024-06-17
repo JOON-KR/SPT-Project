892cb515736793841fb6d556ee20b031fd7d2cd3
prao <pch5521@gmail.com>
2024-06-18 00:42:21 +0900

    Refactor: "@Setter 추가(수정 필요)"
    - @Setter가 없으면 entity로 변환되지 않는 버그 발생, 수정 필요

--- dawnary_back/src/main/java/com/sptp/dawnary/member/domain/Member.java
+++ dawnary_back/src/main/java/com/sptp/dawnary/member/domain/Member.java
@@ -11,9 +11,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "MEMBER")
+@Setter
 @Getter
 @Builder
 @AllArgsConstructor
