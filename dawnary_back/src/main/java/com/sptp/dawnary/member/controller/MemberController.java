fc0e9b3ebd2ea7d879fbac9da91061863daca67e
prao <pch5521@gmail.com>
2024-06-19 15:49:53 +0900

    Chore: "@CrossOrigin 추가 및 기타 파일 삭제"

--- dawnary_back/src/main/java/com/sptp/dawnary/member/controller/MemberController.java
+++ dawnary_back/src/main/java/com/sptp/dawnary/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.sptp.dawnary.member.controller;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.sptp.dawnary.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin("*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
