deee24298a7e203e39398224a836724d626acc9f
seohyun kwon <seohyunkwon98@gmail.com>
2024-06-19 13:40:28 +0900

    Fix: "Series 수정"

--- dawnary_back/src/main/java/com/sptp/dawnary/series/domain/Series.java
+++ dawnary_back/src/main/java/com/sptp/dawnary/series/domain/Series.java
@@ -1,22 +1,49 @@
 package com.sptp.dawnary.series.domain;
 
-
-import java.util.Date;
-
 import com.sptp.dawnary.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
 @Getter
+@Setter
+@ToString
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Series {
-	   private long id;
-	    private Member member;
-	    private String title;
-	    private int status;
-	    private String imagePath;
-	    private int viewCnt;
-	    private Date regDate;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
+	@Column(name = "title", nullable = false, length = 50)
+	private String title;
+
+	@Column(name = "image_path", nullable = false)
+	private String imagePath;
+
+	@Column(name = "status", nullable = false)
+	@ColumnDefault("1")
+	private int status;
+
+	@Column(name = "view_cnt", nullable = false)
+	@ColumnDefault("0")
+	private long viewCnt;
+
+	@Column(name = "reg_date", nullable = false, updatable = false)
+	private LocalDateTime regDate;
+
+	@PrePersist
+	protected void onCreate() {
+		regDate = LocalDateTime.now();
+	}
 }
