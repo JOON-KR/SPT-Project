b35bf8cc975c5e33f28ec625d97e15c7672d5cf5
dawoon <52641434+wwhynott@users.noreply.github.com>
2024-06-19 11:41:18 +0900

    Feat: "달력페이지 관련 컴포넌트 route 링크 추가"
    달력, 사이드슬라이드 컴포넌트 추가

--- dawnary_front/src/App.jsx
+++ dawnary_front/src/App.jsx
@@ -4,6 +4,9 @@ import MyPage from "./components/main/MyPage";
 import Regist from "./components/main/Regist";
 import UserPage from "./components/main/UserPage";
 import KakaoLogin from "./components/main/KakaoLogin";
+import MainCalendar from "./components/calendar/MainCalendar";
+import Calendar from "./components/calendar/Calendar";
+import SideSlide from "./components/calendar/SideSlide";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -15,6 +18,9 @@ function App() {
       <Route path="/myPage" element={<MyPage />} />
       <Route path="/userPage" element={<UserPage />} />
       <Route path="/kakaoLogin" element={<KakaoLogin />} />
+      <Route path="/mainCalendar" element={<MainCalendar />} />
+      <Route path="/calendar" element={<Calendar />} />
+      <Route path="/sideSlide" element={<SideSlide />} />
     </Routes>
   );
 }
