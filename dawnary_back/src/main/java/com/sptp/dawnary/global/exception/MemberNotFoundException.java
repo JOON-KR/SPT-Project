415697ac125ec934426cf46277e2fcb9f30f6407
prao <pch5521@gmail.com>
2024-06-20 23:43:38 +0900

    Refactor: "예외 구조 변경"

--- dawnary_back/src/main/java/com/sptp/dawnary/global/exception/MemberNotFoundException.java
+++ dawnary_back/src/main/java/com/sptp/dawnary/global/exception/MemberNotFoundException.java
@@ -2,6 +2,12 @@ package com.sptp.dawnary.global.exception;
 
 public class MemberNotFoundException extends IllegalArgumentException{
 
+    private static final String MESSAGE = "멤버가 존재하지 않습니다.";
+
+    public MemberNotFoundException() {
+        super(MESSAGE);
+    }
+
     public MemberNotFoundException(String message) {
         super(message);
     }
