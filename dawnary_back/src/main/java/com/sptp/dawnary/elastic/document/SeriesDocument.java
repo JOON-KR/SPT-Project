d4d7139779402484b31e7a5027b2f73cd5478c59
joon <yyj4148@naver.com>
2024-06-21 01:12:34 +0900

    feat: '자동 완성 쿼리'

--- dawnary_back/src/main/java/com/sptp/dawnary/elastic/document/SeriesDocument.java
+++ dawnary_back/src/main/java/com/sptp/dawnary/elastic/document/SeriesDocument.java
@@ -6,6 +6,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.InnerField;
+import org.springframework.data.elasticsearch.annotations.MultiField;
 
 import com.sptp.dawnary.series.domain.Series;
 
@@ -24,18 +26,18 @@ import lombok.Setter;
 public class SeriesDocument {
 
     @Id
-    private long id;
-   // @MultiField(mainField = @Field(type = FieldType.Text, analyzer = "ngram"),
-    //        otherFields = {
-     //           @InnerField(suffix = "exact", type = FieldType.Keyword),
-     //           @InnerField(suffix = "autocomplete", type = FieldType.Text, analyzer = "autocomplete_analyzer")
-     //       })
+    private String id;
+    @MultiField(mainField = @Field(type = FieldType.Text, analyzer = "ngram"),
+            otherFields = {
+                @InnerField(suffix = "exact", type = FieldType.Keyword),
+                @InnerField(suffix = "autocomplete", type = FieldType.Text, analyzer = "autocomplete_analyzer")
+            })
     private String memberName;
-  //  @MultiField(mainField = @Field(type = FieldType.Text, analyzer = "ngram"),
-     //       otherFields = {
-      //          @InnerField(suffix = "exact", type = FieldType.Keyword),
-      //          @InnerField(suffix = "autocomplete", type = FieldType.Text, analyzer = "autocomplete_analyzer")
-       //     })
+    @MultiField(mainField = @Field(type = FieldType.Text, analyzer = "ngram"),
+            otherFields = {
+                @InnerField(suffix = "exact", type = FieldType.Keyword),
+                @InnerField(suffix = "autocomplete", type = FieldType.Text, analyzer = "autocomplete_analyzer")
+            })
     private String title;
     private int status;
     private String imagePath;
@@ -45,8 +47,8 @@ public class SeriesDocument {
     // 자동 완성을 위한 필드
 
     public static SeriesDocument from(Series series) {
+    	System.out.println(series.getMember().toString());
         return SeriesDocument.builder()
-                .id(series.getId())
                 .memberName(series.getMember().getName())
                 .title(series.getTitle())
                 .status(series.getStatus())
