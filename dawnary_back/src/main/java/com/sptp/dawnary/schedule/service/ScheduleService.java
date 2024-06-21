c28023933f1fc63c20412fbd7f68784bea74c52d
seohyun kwon <seohyunkwon98@gmail.com>
2024-06-21 13:50:37 +0900

    Refactor: "Schedule record 전환"

--- dawnary_back/src/main/java/com/sptp/dawnary/schedule/service/ScheduleService.java
+++ dawnary_back/src/main/java/com/sptp/dawnary/schedule/service/ScheduleService.java
@@ -8,11 +8,11 @@ import com.sptp.dawnary.location.repository.LocationRepository;
 import com.sptp.dawnary.member.domain.Member;
 import com.sptp.dawnary.member.repository.MemberRepository;
 import com.sptp.dawnary.schedule.domain.Schedule;
-import com.sptp.dawnary.schedule.dto.ScheduleDto;
+import com.sptp.dawnary.schedule.dto.ScheduleRequest;
+import com.sptp.dawnary.schedule.dto.ScheduleResponse;
 import com.sptp.dawnary.schedule.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,46 +28,43 @@ public class ScheduleService {
     private final MemberRepository memberRepository;
     private final LocationRepository locationRepository;
 
-    private final ModelMapper modelMapper;
-
     // 스케줄 등록
-    public ScheduleDto saveSchedule(ScheduleDto scheduleDto) {
+    public ScheduleResponse saveSchedule(ScheduleRequest scheduleRequest) {
         Member member = getMember();
-        Schedule schedule = modelMapper.map(scheduleDto, Schedule.class);
+        Schedule schedule = Schedule.toEntity(scheduleRequest);
         schedule.setMember(member);
         schedule.setLocation(null);
 
         scheduleRepository.save(schedule);
 
-        Location location = modelMapper.map(scheduleDto.getLocation(), Location.class);
+        Location location = Location.toEntity(scheduleRequest.locationRequest());
         location.setSchedule(schedule);
 
         Location savedLocation = locationRepository.save(location);
         schedule.setLocation(savedLocation);
         scheduleRepository.save(schedule);
 
-        return modelMapper.map(schedule, ScheduleDto.class);
+        return ScheduleResponse.toResponse(schedule);
     }
 
     // 스케줄 수정
-    public ScheduleDto updateSchedule(Long scheduleId, ScheduleDto scheduleDto) {
+    public ScheduleResponse updateSchedule(Long scheduleId, ScheduleRequest scheduleRequest) {
 
-        Schedule schedule = modelMapper.map(scheduleDto, Schedule.class);
+        Schedule schedule = Schedule.toEntity(scheduleRequest);
         schedule.setMember(getMember());
 
         if (scheduleRepository.existsById(scheduleId)) {
             schedule.setId(scheduleId);
-            Schedule updatedSchedule = scheduleRepository.save(schedule);
-            return modelMapper.map(updatedSchedule, ScheduleDto.class);
+            scheduleRepository.save(schedule);
+            return ScheduleResponse.toResponse(schedule);
         }
 
-        throw new ScheduleNotFoundException("존재하지 않는 스케줄 입니다.");
+        throw new ScheduleNotFoundException();
 
     }
 
     // 스케줄 삭제
     public boolean deleteSchedule(Long scheduleId) {
-
         if (scheduleRepository.existsById(scheduleId)) {
             scheduleRepository.deleteById(scheduleId);
             return true;
@@ -78,24 +75,24 @@ public class ScheduleService {
     }
 
     // 스케줄 목록 조회
-    public List<ScheduleDto> findAllSchedules() {
+    public List<ScheduleResponse> findAllSchedules() {
         Long memberId = MemberInfo.getMemberId();
-        List<Schedule> schedules = scheduleRepository.findAllOrderByDateDesc(memberId);
+        List<Schedule> schedules = scheduleRepository.findByMemberId(memberId);
         return schedules.stream()
-                .map(schedule -> modelMapper.map(schedule, ScheduleDto.class))
+                .map(ScheduleResponse::toResponse)
                 .toList();
     }
 
     // 특정 스케줄 조회
-    public ScheduleDto findSchedule(Long scheduleId) {
+    public ScheduleResponse findSchedule(Long scheduleId) {
         Schedule schedule = scheduleRepository.findById(scheduleId)
-                .orElseThrow(() -> new ScheduleNotFoundException("존재하지 않는 스케줄 입니다."));
-        return modelMapper.map(schedule, ScheduleDto.class);
+                .orElseThrow(() -> new ScheduleNotFoundException());
+        return ScheduleResponse.toResponse(schedule);
     }
 
     private Member getMember() {
         Long memberId = MemberInfo.getMemberId();
         return memberRepository.findById(memberId)
-                .orElseThrow(() -> new MemberNotFoundException("멤버가 존재하지 않습니다."));
+                .orElseThrow(() -> new MemberNotFoundException());
     }
 }
