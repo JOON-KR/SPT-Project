6ea81aed14583c466534c20dd5bc36eaa7e093cd
seohyun kwon <seohyunkwon98@gmail.com>
2024-06-24 10:32:10 +0900

    Fix: "Diary repository 수정"

--- dawnary_back/src/main/java/com/sptp/dawnary/diary/repository/DiaryRepository.java
+++ dawnary_back/src/main/java/com/sptp/dawnary/diary/repository/DiaryRepository.java
@@ -3,6 +3,7 @@ package com.sptp.dawnary.diary.repository;
 import com.sptp.dawnary.diary.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,7 +12,7 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
     List<Diary> findByMemberId(Long memberId);
 
     @Query("SELECT d FROM Diary d WHERE d.member.id = :memberId AND d.status = 1")
-    List<Diary> findOtherUserDiaries(Long memberId);
+    List<Diary> findOtherUserDiaries(@Param("memberId") Long memberId);
 
     @Query("SELECT d FROM Diary d, Follow f WHERE d.member.id = f.following.id AND f.follower.id = :memberId AND d.status = 1")
     List<Diary> findFollowUserDiaries(Long memberId);
